c74ce1e5c6588856963367c68efc692038e16d94
Ndukwe <pndukwe824@gmail.com>
2026-01-29 00:44:46 +0100

    error update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit';
 import { sdk } from '@farcaster/miniapp-sdk';
 import App from './app';
-import ErrorBoundary from './components/ErrorBoundary';
 import { config } from './config/wagmi';
 import '@rainbow-me/rainbowkit/styles.css';
 import './index.css';
@@ -40,9 +39,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             borderRadius: 'large',
           })}
         >
-          <ErrorBoundary>
-            <App />
-          </ErrorBoundary>
+          <App />
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
